8c9537af0a400d675373d53ca14404fd7726d2b8
wwrowe@gmail.com <wwrowe@gmail.com>
2015-10-23 16:15:23 -0400

    test upload the HW.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## To cache the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+		solve(x)
 
 }
 
 
-## Write a short comment describing this function
+## If the matrix is the same, get the inverse from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getmatrix
+		if (!is.null(m)){
+		return(m)
+		}
+		
 }
